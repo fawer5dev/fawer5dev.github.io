b90c28922ff76bf26575df541ee73639e722846a
fawer5dev <fawer5@gmail.com>
2024-07-13 22:26:07 +1000

    Changes to be committed: 	modified:   src/assets/images/PasswGen.webp 	new file:   src/assets/images/Screenshot 2024-07-13 221855.png 	new file:   src/assets/images/website.webp 	modified:   src/components/Footer.astro 	modified:   src/utils/projects.js

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -7,7 +7,18 @@ import LifeInsureEase from "../assets/images/lifeinsureease.webp";
 import Portfolio from "../assets/images/portfolio.webp"
 import CartManagementSystem from "../assets/images/cart-management-system.webp"
 import PasswGen from "../assets/images/PasswGen.webp"
+import Website from "../assets/images/website.webp"
 const data = [
+    {
+        name: "My Portfolio",
+        type: "Website  ",
+        url: "/",
+        github: "https://github.com/fawer5dev/fawer5dev.github.io",
+        image: Website,
+        slug: "website",
+        description: "My Portfolio",
+        tech: ['Astro', 'Html', 'JavaScript', 'TypeScript', 'Css']        
+    },
     {
         name: "Password Generator",
         type: "WebApp",
@@ -16,9 +27,8 @@ const data = [
         image: PasswGen,
         slug: "password-generator",
         description: "This code is a JavaScript file that generates random passwords based on user preferences, and provides an option to copy the password to the user's clipboard.",
-        tech: ['HTML', 'Javascript', 'Css']
-        
-    },    
+        tech: ['HTML', 'JavaScript', 'Css'],
+    },        
 ]
 
 export function getData(){
